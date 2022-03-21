28df8935829f07b2940a4c2851e55ea11854e85a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-21 12:51:44 -0400

    Navbar: flex examples

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,5 +1,22 @@
+import styles from "../../styles/Navbar.module.css";
 import React from "react";
+import Image from "next/image";
+import { AiOutlinePhone } from "react-icons/ai";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <AiOutlinePhone color="orange" size="32px" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>012 34 56 67</div>
+        </div>
+      </div>
+      <div className={styles.item}>center</div>
+      <div className={styles.item}>right</div>
+    </div>
+  );
 };
