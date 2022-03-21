e00d8d1709416201d73603e1dd6d628dee735c50
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-21 13:38:45 -0400

    Navbar: styling the center and left section

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,6 +2,7 @@ import styles from "../../styles/Navbar.module.css";
 import React from "react";
 import Image from "next/image";
 import { AiOutlinePhone } from "react-icons/ai";
+import { BsCart2 } from "react-icons/Bs";
 
 export const Navbar = () => {
   return (
@@ -15,8 +16,25 @@ export const Navbar = () => {
           <div className={styles.text}>012 34 56 67</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>HomePage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+
+          <Image src="/img/logo.png" width="100px" height="69px" alt="" />
+
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <BsCart2 size="30px" color="white" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
